c439dca745fae534ea80ebfe7628bf01883596a1
Liang Xing Wong <samoos9032@gmail.com>
2025-07-06 15:55:25 +0900

    fix

--- utils/utils_test.go
+++ utils/utils_test.go
@@ -46,22 +46,6 @@ func generateJSONWithQuotes(size int, quotePositions []int) []byte {
 	return data
 }
 
-// min returns the minimum of two integers
-func min(a, b int) int {
-	if a < b {
-		return a
-	}
-	return b
-}
-
-// max returns the maximum of two integers
-func max(a, b int) int {
-	if a > b {
-		return a
-	}
-	return b
-}
-
 // ============================================================================
 // ZERO-ALLOCATION TYPE CONVERSION TESTS
 // ============================================================================
