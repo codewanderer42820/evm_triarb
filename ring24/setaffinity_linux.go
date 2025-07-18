ea69d83f2bfe6e6a23bbecdcc875d9fab3be2c7e
Liang Xing Wong <samoos9032@gmail.com>
2025-07-18 13:00:53 +0900

    fix

--- ring24/setaffinity_linux.go
+++ ring24/setaffinity_linux.go
@@ -32,7 +32,7 @@ import (
 // cpuMasks contains pre-computed affinity masks for cores 0-63.
 // Each mask has a single bit set corresponding to the target CPU.
 // Pre-computation eliminates runtime bit manipulation overhead.
-var cpuMasks = [...][1]uintptr{
+var cpuMasks = [...][1]uint64{
 	{1 << 0}, {1 << 1}, {1 << 2}, {1 << 3}, {1 << 4}, {1 << 5}, {1 << 6}, {1 << 7},
 	{1 << 8}, {1 << 9}, {1 << 10}, {1 << 11}, {1 << 12}, {1 << 13}, {1 << 14}, {1 << 15},
 	{1 << 16}, {1 << 17}, {1 << 18}, {1 << 19}, {1 << 20}, {1 << 21}, {1 << 22}, {1 << 23},
