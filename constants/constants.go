7d1865e2190f720b4ed6e9db1485ad7fb60e28ba
Liang Xing Wong <samoos9032@gmail.com>
2025-07-21 05:28:55 +0900

    fix

--- constants/constants.go
+++ constants/constants.go
@@ -112,7 +112,7 @@ const (
 	OptimalBatchSize         = uint64(8_000)
 	MinBatchSize             = uint64(1)
 	MaxLogSliceSize          = 1_000_000
-	DefaultConnections       = 3
+	DefaultConnections       = 1
 )
 
 // ═══════════════════════════════════════════════════════════════════════════════════════════════
